8e6dbdc6c2c25c83076b457aef05c9f544281422
Vladimir Daniyan <v.daniyan@gmail.com>
2016-04-10 03:15:32 -0400

    Get the names of games available for direct streaming
    Instead of launching steam, we can get the list of available game
    dynamically and launch them directly

--- moonlighty.py
+++ moonlighty.py
@@ -1,7 +1,10 @@
 #!venv/bin/python
-from flask import Flask, render_template
-from subprocess import Popen, PIPE
+from flask import Flask, render_template, Response, jsonify
 from flask.ext.script import Manager, Server, Command, Option
+from subprocess import Popen, PIPE
+import re
+import time
+import sys
 
 # http://stackoverflow.com/questions/14566570/how-to-use-flask-script-and-gunicorn
 class GunicornServer(Command):
@@ -35,13 +38,36 @@ manager.add_command("gunicorn", GunicornServer())
 def index():
     return render_template('index.html')
 
-@app.route('/launch')
-def moonlight():
-    logfile = open('/var/log/moonlighty/moonlight.log', 'w')
-    cmd = ['sudo', '-u', 'pi', 'moonlight', 'stream', '-app', 'Steam', '-mapping', '/home/pi/xbox.conf', '-1080', '-30fps']
-    p = Popen(cmd, stdout=logfile, stderr=logfile)
-    output = p.communicate()
-    return output
+@app.route('/list')
+def game_list():
+
+    def moonlight_games():
+        #logfile = open('/var/log/moonlighty/moonlight.log', 'w')
+        #cmd = ['sudo', '-u', 'pi', 'moonlight', 'stream', '-app', 'Steam', '-mapping', '/home/pi/xbox.conf', '-1080', '-30fps']
+        moon_list = Popen(['sudo', '-u', 'pi', 'moonlight', 'list'], stdout=PIPE)
+        moon_list_sorted = Popen(['sed', '1,4d'], stdin=moon_list.stdout, stdout=PIPE)
+        moon_list.stdout.close()
+        while moon_list_sorted.poll() is None:
+            time.sleep(2)
+        output, error = moon_list_sorted.communicate()
+
+        # moonlight returns a numbered list of games, we want to parse that
+        pattern = '(?<=\d\.\s).*$'
+        steam_games = []
+        if error is None:
+            for game in output.splitlines():
+                for match in re.findall(pattern, game):
+                    steam_games.append(match)
+            return steam_games
+        else:
+            raise IOError(error)
+
+    return Response(moonlight_games(), mimetype='text/html')
+
+#TODO: create dynamic routes based on game list
+# allowing games to be started directly
+@app.route('/list/<game>')
+def start_game():
 
 if __name__ == '__main__':
     manager.run()
